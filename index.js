699ad726e8f9165b3adccf47b63badd520240c81
PrashanthSubramani <prashanthsubramani1699@gmail.com>
2024-02-01 20:14:18 +0530

    minor CHanges in backend 9

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const app = express();
 const cookieParser = require("cookie-parser");
 require('dotenv').config();
 
-app.set('views', __dirname + '/views');
-app.set('view engine','ejs');
+app.set('views', './views');
+app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended:false}));
 
 app.listen(process.env.PORT,()=>{
